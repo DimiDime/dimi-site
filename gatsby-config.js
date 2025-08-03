ea7e8eab3a52f72ab25358d86ac0e3757fe40c0d
DimiDime <ilev997@gmail.com>
2025-08-03 19:40:49 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Dimi Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
